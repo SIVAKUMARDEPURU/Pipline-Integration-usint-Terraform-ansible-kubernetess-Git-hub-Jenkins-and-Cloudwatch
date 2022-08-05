cddf63cd77778f56ed1074cffa4a9a8fd403d45e
SIVAKUMARDEPURU <110371695+SIVAKUMARDEPURU@users.noreply.github.com>
2022-08-05 11:52:15 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,8 +12,7 @@ resource "aws_instance" "instance1" {
   root_block_device {
     volume_size = var.volume_size
   }
-  user_data = filebase64(var.user_data)
-  tags={
+   tags={
     Name= "instance-${count.index}"
   }
 }
